9b08bebfae72c56b3e7761ddfffaeddf351818fc
Andrew Udvare <audvare@gmail.com>
2026-09-10 13:56:21 -0400

    cruft: update

--- tests/test_temporalsoften.py
+++ tests/test_temporalsoften.py
@@ -2,8 +2,9 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from scenechange import TemporalSoften
 import vapoursynth as vs  # type: ignore[import-untyped]
+
+from scenechange import TemporalSoften
 
 if TYPE_CHECKING:
     from pytest_mock import MockerFixture
